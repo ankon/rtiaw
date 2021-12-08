70e4d780bd2dbb575553ec1e13c9a7ff7eeb5f37
Andreas Kohn <andreas.kohn@gmail.com>
2021-12-08 12:05:05 +0100

    Optimize the writing of the image contents
    Instead of writing each byte separately we now prepare a single buffer, and write that.
    The buffer itself is filled from a pre-calculated look-up-table of the color values
    (0...255) to the actual bytes that must be written. This includes padding, so when filling
    the buffer we can exactly point to the index of a specific component.

--- src/ppm.ts
+++ src/ppm.ts
@@ -30,6 +30,25 @@ export function ppm(
 			: gamma === 2
 			? (c) => Math.sqrt(c)
 			: (c) => Math.pow(c, 1 / gamma);
+
+	const bytesPerComponent = Math.ceil(Math.log10(maxColor));
+	/**
+	 * Precalculated mapping for color component value to actual buffer to write
+	 *
+	 * Note that the values are pre-padded already!
+	 */
+	const lut: Buffer[] = new Array(maxColor + 1);
+	for (let i = 0; i < maxColor + 1; i++) {
+		lut[i] = Buffer.from(String(i).padStart(bytesPerComponent));
+	}
+
+	/**
+	 * Bytes needed to write a single color component
+	 *
+	 * This has 4 bytes per color component: space/newline after it.
+	 */
+	const outputBytesPerComponent = bytesPerComponent + 1;
+
 	return {
 		get width() {
 			return width;
@@ -38,18 +57,32 @@ export function ppm(
 			return height;
 		},
 		writeLine(line) {
+			/** Buffer for constructing a single line to write */
+			// We could reuse this, IFF we ensure that the previous content has been written first!
+			const lineBuffer = Buffer.alloc(
+				width * 3 * outputBytesPerComponent
+			);
+
+			let lineBufferIndex = 0;
 			for (let i = 0; i < line.length; i++) {
 				const c = line[i];
 				for (let j = 0; j < 3; j++) {
 					const gammaCorrected = gammaCorrect(c[j]);
-					const value = Math.floor(255 * gammaCorrected);
-					out.write(String(value));
-					out.write(' ');
+					const value = Math.floor(maxColor * gammaCorrected);
+					const bytes = lut[value];
+					lineBuffer.set(bytes, lineBufferIndex);
+					lineBuffer.set(
+						j === 2 ? [10] : [32],
+						lineBufferIndex + bytesPerComponent
+					);
+
+					lineBufferIndex += outputBytesPerComponent;
 				}
-				out.write('\n');
 			}
-			out.write('\n');
+			out.write(lineBuffer);
 		},
-		finish() {},
+		async finish() {
+			out.end();
+		},
 	};
 }
